b3ad25130bcedcbaf37e93f1f99f85bbcf82aefe
MatejKopera <matej.kopera@asseco-ceit.com>
2025-04-17 11:19:55 +0200

    Add project files.

---
+++ JsonComparator.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace MapJsonComparator;
+
+public static class JsonComparator
+{
+    public static void CompareJsonTokens(JToken a, JToken b, string path, List<string> diffs)
+    {
+        if (a.Type != b.Type)
+        {
+            diffs.Add($"Different types: {path}: ({a.Type}, {b.Type})"); // should not happen
+            return;
+        }
+
+        switch (a.Type)
+        {
+            case JTokenType.Object:
+                CompareJsonObjects((JObject)a, (JObject)b, path, diffs);
+                break;
+
+            case JTokenType.Array:
+                CompareJsonArrays((JArray)a, (JArray)b, path, diffs);
+                break;
+
+            default:
+                CompareJsonValues((JValue)a, (JValue)b, path, diffs);
+                break;
+        }
+    }
+
+    private static void CompareJsonArrays(JArray arrA, JArray arrB, string path, List<string> diffs)
+    {
+        // use "id" if possible
+        bool hasIdA = arrA.All(t => t.Type == JTokenType.Object && ((JObject)t).Property("id")?.Value.Type == JTokenType.String);
+        bool hasIdB = arrB.All(t => t.Type == JTokenType.Object && ((JObject)t).Property("id")?.Value.Type == JTokenType.String);
+
+        if (hasIdA && hasIdB)
+        {
+            Func<JObject, string> getKey = static o =>
+            {
+                if (o.Property("nodeId") != null)
+                {
+                    return o.Value<string>("nodeId");
+                }
+                else
+                {
+                    return o.Value<string>("id");
+                }
+            };
+            var dictA = arrA.Cast<JObject>().ToDictionary(getKey);
+            var dictB = arrB.Cast<JObject>().ToDictionary(getKey);
+            var allIds = new HashSet<string>(dictA.Keys);
+            allIds.UnionWith(dictB.Keys);
+
+            foreach (var id in allIds)
+            {
+                var childPath = $"{path}[id={id}]";
+                if (!dictA.ContainsKey(id))
+                    diffs.Add($"added: {childPath}");
+                else if (!dictB.ContainsKey(id))
+                    diffs.Add($"removed: {childPath}");
+                else
+                    CompareJsonObjects(dictA[id], dictB[id], childPath, diffs);
+            }
+        }
+        else
+        {
+            // Fallback: if id not exists, compare by index
+            int max = Math.Max(arrA.Count, arrB.Count);
+            for (int i = 0; i < max; i++)
+            {
+                var childPath = $"{path}[{i}]";
+                if (i >= arrA.Count)
+                    diffs.Add($"added: {childPath}");
+                else if (i >= arrB.Count)
+                    diffs.Add($"removed: {childPath}");
+                else
+                    CompareJsonTokens(arrA[i], arrB[i], childPath, diffs);
+            }
+        }
+    }
+
+    private static void CompareJsonValues(JValue valA, JValue valB, string path, List<string> diffs)
+    {
+        if (!object.Equals(valA, valB))
+            diffs.Add($"changed: {path}: (old=\"{valB}\", new=\"{valA}\")");
+    }
+
+    private static void CompareJsonObjects(JObject objA, JObject objB, string path, List<string> diffs)
+    {
+        var allKeys = new HashSet<string>(objA.Properties().Select(p => p.Name));
+        allKeys.UnionWith(objB.Properties().Select(p => p.Name));
+        foreach (var key in allKeys)
+        {
+            var childPath = string.IsNullOrEmpty(path) ? key : $"{path}.{key}";
+            if (!objA.TryGetValue(key, out var va))
+                diffs.Add($"added: {childPath}");
+            else if (!objB.TryGetValue(key, out var vb))
+                diffs.Add($"removed: {childPath}");
+            else
+                CompareJsonTokens(va, vb, childPath, diffs);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using MapJsonComparator;
+using Newtonsoft.Json.Linq;
+
+string oldFile;
+string newFile;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Enter file path to old json.");
+    Console.Write("> ");
+    oldFile = Console.ReadLine();
+    Console.WriteLine("Enter file path to new json.");
+    Console.Write("> ");
+    newFile = Console.ReadLine();
+    Console.WriteLine();
+}
+else
+{
+    oldFile = args[0];
+    newFile = args[1];
+}
+
+try
+{
+    JToken oldJson = JToken.Parse(File.ReadAllText(oldFile));
+    JToken newJson = JToken.Parse(File.ReadAllText(newFile));
+
+    var diffs = new List<string>();
+
+    // Spusti rekurzívne porovnanie
+    JsonComparator.CompareJsonTokens(oldJson, newJson, "", diffs);
+
+    // output
+    if (diffs.Count != 0)
+    {
+        diffs.Sort();
+        var outputNodes = diffs.Where(x => x.Contains("nodes"));
+        var outputEdges = diffs.Where(x => x.Contains("edges"));
+        var outputOthers = diffs.Where(x => !x.Contains("nodes") && !x.Contains("edges"));
+
+        var output = outputNodes
+            .Concat(outputEdges)
+            .Concat(outputOthers)
+            .ToList();
+
+        output.ForEach(Console.WriteLine);
+    }
+    else
+    {
+        Console.WriteLine("No changes.");
+    }
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Chyba: Jeden alebo oba súbory neboli nájdené.");
+}
+catch (IOException e)
+{
+    Console.WriteLine($"Error reading file: {e.Message}");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"An unexpected error occurred: {e.Message}");
+}
+finally
+{
+    Console.WriteLine("\nEnter 'exit' to terminate the program:");
+    string input;
+    do
+    {
+        input = Console.ReadLine();
+    } while (input?.ToLower() != "exit");
+}
